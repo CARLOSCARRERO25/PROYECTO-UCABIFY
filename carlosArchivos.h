af04594469996ba7d9a25aae9ce925b284944642
Carlos Carrero Lauriz <164264628+CARLOSCARRERO25@users.noreply.github.com>
2024-05-17 23:32:58 -0400

    Archivos con funcione para canciones

--- carlosArchivos.h
+++ carlosArchivos.h
@@ -1,15 +1,73 @@
 #include<iostream>
 #include<stdlib.h>
 #include<fstream>
-//#include<carloslistas.h>
-#define NombreArchivo "NOMBRE DEL ARCHIVO"
+#include<LibreriaUsuarios.h>
+#define NombreArchivo "ARCHIVO_PROYECTO.txt"
+#include<LibreriaHistorial.h>
+#include<LibreriaCanciones.h>
+
 
 #define ComandoSalto "\r\n"
-
 using namespace std;
 
+struct Canciones{
+    int Identificador;
+    string Nombre_Cancion;
+    string Artista;
+    string Genero;
+    int Year;
+    int Contador_Reproducciones;
+    bool Like;
+    Canciones *Next;
 
-void ArchivoVerificacion(){}
+};
+typedef Canciones *PtrCanciones;
+Canciones Cancion;
+
+struct Usuarios{
+    int Codigo_Identificador;
+    string Nombre_Usuario;
+    string Correo;
+    short Edad;
+    string Password;
+    string Pais;
+    Canciones *PtrHistorial;
+    int ContadorHistorial;
+    Usuarios *Next;
+    
+};
+
+typedef Usuarios *PtrUsuarios;
+
+
+
+void conversor_campos_canciones(PtrCanciones &lista,int conteo, string linea){
+int x;
+    switch (conteo)
+    {
+    case 1:
+        x=stoi(linea);
+        lista->Identificador=x;
+        break;
+
+    case 2:
+        lista->Nombre_Cancion =linea;
+        break;
+
+    case 3:
+        lista->Artista=linea;
+        break;
+    case 4:
+    lista->Genero=linea;break;
+    case 5:
+        x=stoi(linea);
+        lista->Year=x;
+        break;
+    default:
+        break;
+    }
+}
+
 
 void ArchivosEscribir(){
     ofstream archivo;
@@ -20,57 +78,93 @@ void ArchivosEscribir(){
     archivo.close();
 }
 
-/*void fArchivo_LEERTexto(){
-    ofstream archivo;
-    string texto;
 
-    archivo.open("NombreArchivo",ios::app);
-    if(archivo.fail()) {
-        cout<<"NO SE ABRIO ARCHIVO";}
-//  cout<<"ingrese datos";
-//  getline(cin,texto);
-    archivo<<"PRUEBAAA TILIN AHHH";
-
-    archivo.close();
-}*/
-
-void fArchivo_ImprimirContenido(){
+void fArchivo_LeerCanciones(PtrCanciones &lista){
     ifstream archivo;
-    string texto,linea;
+    Canciones *ptraux=lista;
+    string texto,linea,linea2;
+    int conteo=0;
+    char letra;
+
     archivo.open("NombreArchivo");
     //mientras haya contenido en el archivo se tomara linea por linea el contenido
-    while(getline(archivo,linea));{
-        
+    while(getline(archivo,linea) ){
 
-        texto=texto+linea"\n";
-        lista = Lista
+
+        for(char letra:linea){
+
+            linea2= linea2+letra;
+
+            if(letra == '\t'){
+                conversor_campos_canciones(ptraux,conteo,linea2);
+                conteo++;
+                linea2="";
+                ptraux = ptraux->Next;
+            }
+        }
+
+        texto=texto+linea+"\n" ;
+        ptraux=ptraux->Next;
+
     }
+
     cout<< texto << endl;
     archivo.close();
 }
 
-void fArchivo_Guardar(string texto){
-    ofstream archivo("NombreArchivo");
+
+void fArchivo_MeterEnArchivoCanciones(PtrCanciones &Lista){
+    ifstream archivo("NombreArchivo");
     archivo.open("NombreArchivo");
+    string estrin;
+    string linea;
 
-    archivo<< /*TEXTOO AHH*/ ;
+    while (Lista)
+    {
 
-    archivo.close;
-
-
-}
-
-void fArchivo_CopiarContenido(){
-    ifstream archivo;
-    string texto;
-    archivo.open("NombreArchivo");
-    //mientras haya contenido en el archivo se tomara linea por linea el contenido
-    while(getline(archivo,texto));{
-
-        /*copiar desde el archivo hasta la estructura de listas {canciones,historial,usuario}*/
-        // \n (para salto de linea)
+    estrin=to_string(Lista->Identificador);
+    linea = linea + estrin;
+    linea = linea + Lista->Nombre_Cancion;
+    linea = linea + Lista->Artista;
+    linea = linea + Lista->Genero;
+    estrin = to_string	(Lista->Year);
+    linea = linea + estrin;
+    Lista=Lista->Next;
 
     }
+        
+        archivo>>linea;
+        
+    archivo.close();
 
+
+}
+
+
+void fArchivoLeerUsuario(PtrUsuarios &lista){
+    string linea,linea2;
+    PtrUsuarios UsuAux=lista;
+    PtrCanciones CanAux;
+    ifstream archivo("ArchivoHistorial.txt");
+    archivo.open("ArchivoHistorial.txt");
+
+    CanAux= UsuAux->PtrHistorial;
+    linea= UsuAux->Codigo_Identificador + ' / ';
+
+    while(UsuAux){
+        while(CanAux){
+            linea2=to_string(CanAux->Identificador);
+            if(CanAux->Next)
+                linea+= linea2+'-';
+            else 
+                linea+= linea2+'\n';
+            CanAux=CanAux->Next;
+        }
+        UsuAux=UsuAux->Next;
+    }
+//
     archivo.close();
 }
+
+
+
