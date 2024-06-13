ef7adf538cc6a045c4e1a9413b0043f2a0db30ef
Cv1ll4 <cmsevilla.22@est.ucab.edu.ve>
2024-06-12 23:25:43 -0400

    El contador de canciones ahora se modifica cuando un usuario reproduce una canción

--- Proyecto_UCABIFY.cpp
+++ Proyecto_UCABIFY.cpp
@@ -265,16 +265,17 @@ int main(){
                 }while(!EsNumero(Seleccion2));
                 MsgdeError(3, Seleccion2);
             }while(stoi(Seleccion2)<=0 || stoi(Seleccion2)>ContadorCanciones);
-            Codigo = BuscarCancion(ListaCanciones, stoi(Seleccion2))->Identificador;
 
-            if(BuscarHistorial(ListaUsuarios, stoi(Seleccion1), Codigo)){
+            if(BuscarHistorial(ListaUsuarios, stoi(Seleccion1), BuscarCancion(ListaCanciones, stoi(Seleccion2))->Identificador)){
                 cout<<"La cancion ya se encuentra en el historial\n";
                 delay(2);
             }else{
                 AgregarHistorial(ListaUsuarios, ListaCanciones,stoi(Seleccion1), stoi(Seleccion2));
             }
-            
+
+            BuscarCancion(ListaCanciones, stoi(Seleccion2))->ContadorReproducciones++;
             ArchivoMeterEnHistorial(ListaUsuarios);
+            ArchivoMeterEnArchivoCanciones(ListaCanciones);
             break;
         default:
             break;
